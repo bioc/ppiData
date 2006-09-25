68583c55ff82a6fd3d9fdee38ccb583613282ed2
Tony Chiang <tchiang@fhcrc.org>
2006-09-25 20:59:02 +0000

    I have made a couple of changes to the viableProteins man page and add a script that generates the graph objects that is in this data file
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/ppiData@131 db2202da-8704-0410-b924-b125b3cd84ef

--- inst/Scripts/genGraphFiles.R
+++ inst/Scripts/genGraphFiles.R
@@ -2,6 +2,10 @@
 
 library(ppiData)
 library(ppiStats)
+library(YEAST)
+yc2s <- as.list(YEASTCOMMON2SYSTEMATIC)
+data(y2hSysGW)
+
 
 bpY2HMats <- lapply(y2hSysGW, bpMatrix, homodimer=TRUE)
 bpY2HGraphs <- lapply(bpY2HMats, genBPGraph)
@@ -14,7 +18,35 @@ data(Krogan)
 data(gavinBP2006)
 data(kroganBPMat2006)
 
-apms$tap <- TAP
+rn <- toupper(rownames(TAP))
+cn <- toupper(colnames(TAP))
+rn1 <- vector(length=length(rn))
+for(i in 1:nrow(TAP)){
+    if(!is.null(yc2s[[rn[i]]])){
+    rn1[i] <- yc2s[[rn[i]]][1]
+    else rn1[i] <- rn[i]
+}
+cn1 <- vector(length = length(cn))
+for(i in 1:ncol(TAP)){
+    if(!is.null(yc2s[[cn[i]]])){
+        cn1[i] <- yc2s[[cn[i]]][1]
+    }
+    else cn1[i] <- cn[i]
+}
+
+which(table(rn1) > 1)
+which(table(cn1) > 1)
+which(cn1 == "YAR042W")
+TAP1 <- TAP
+rownames(TAP1) <- rn1
+colnames(TAP1) <- cn1
+which(TAP1[,199] == 1)
+which(TAP1[,1077] == 1)
+TAP1 <- TAP1[,-1077]
+
+
+
+apms$tap <- TAP1
 apms$hmspci <- HMSPCI
 apms$krogan <- Krogan
 apms$gavin06 <- gavinBP2006
