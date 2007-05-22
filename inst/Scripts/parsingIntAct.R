9f8169eb24757c6600b5353b28f976de40b63c26
Tony Chiang <tchiang@fhcrc.org>
2007-05-22 15:42:23 +0000

    I have updated the parser to filter the ito-core protein interactions
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/ppiData@304 db2202da-8704-0410-b924-b125b3cd84ef

--- inst/Scripts/parsingIntAct.R
+++ inst/Scripts/parsingIntAct.R
@@ -1,46 +1,68 @@
 ##This file will take the parsed IntAct Data and create the 
 ##graph structure that we want:
-parsingIntAct <- function(xmlFile){
+parsingIntAct <- function(xmlFiles, confidence=NULL){
 ###library(Rintact)
-
+  if(class(xmlFiles)!="list"){
+    stop("The input parameter must be a list.")}
+  xmlList <- list()
+  allBaits <- vector()
+  
 ##you need to paste the path to the XML file in the following line
-xml <- xmlFile
+  for(i in 1:length(xmlFiles)){
+    xml <- xmlFiles[[i]]
+    
+    ##"../../../../../../../../downloads/all/psi25/species/yeast_small-03.xml"
+    
+    xmlObject <- psi25interaction(xml)
+    
+    xmlObject <- xmlObject[[1]]
 
-  ##"../../../../../../../../downloads/all/psi25/species/yeast_small-03.xml"
+    if(!is.null(confidence)){
+      ic <- sapply(xmlObject@interactions, function(x) x@confidenceValue) == confidence
+      interaction <- xmlObject@interactions[ic]
+    }
 
-xmlObject <- psi25interaction(xml)
+    ##the following list will store each pull down separately
+    baits <- sapply(interaction, function(x) x@bait)
+    xmlObjectL <- vector("list", length = length(baits))
+    
+    for(i in 1:length(baits)){
+      xmlObjectL[[i]] <- interaction[[i]]@prey
+    }
 
-xmlObject <- xmlObject[[1]]
+    names(xmlObjectL) <- baits
+    
+    xmlList <- c(xmlList, xmlObjectL)
+    allBaits <- c(allBaits, baits)
+    
+  }
+  print(length(xmlList))
+  
+  if(length(allBaits)!=length(unlist(xmlList))){
+    fac <- vector(length=length(unlist(xmlList)))
+    
+    i <- 1
+    j <- 1
+    
+    while(i <= length(fac)){
+      pullDownSize <- length(xmlList[[j]])
+      if(length(i:(i+pullDownSize-1)) == pullDownSize){
+        fac[i:(i+pullDownSize-1)] <-
+          rep(allBaits[j], pullDownSize)}
+      else{stop(paste("bug",j))}
+      i <- pullDownSize+i
+      j <- j+1
+    }
+    
+  }
 
-baits <- sapply(xmlObject@interactions, function(x) x@bait)
-
-##the following list will store each pull down separately
-xmlObjectL <- vector("list", length = length(baits))
-
-for(i in 1:length(baits)){
-  xmlObjectL[[i]] <- xmlObject@interactions[[i]]@prey
-}
-
-names(xmlObjectL) <- baits
-
-fac <- vector(length=length(unlist(xmlObjectL)))
-
-i <- 1
-j <- 1
-
-while(i < length(fac)){
-  pullDownSize <- length(xmlObjectL[[j]])
-  if(length(i:(i+pullDownSize-1)) == pullDownSize){
-    fac[i:(i+pullDownSize-1)] <-
-    rep(baits[j], pullDownSize)}
-  else{stop(paste("bug",j))}
-  i <- pullDownSize+i
-  j <- j+1
-}
-
-##the following list will combine pull downs of the same
-##bait but will still keep multiplicity of prey..i.e. if the
-##prey was found in 2 pulldowns, it will be recored twice 
-xmlObjectList <- split(unlist(xmlObjectL), fac)
-return(xmlObjectList)
+  else{
+    fac = allBaits
+  }
+  
+  ##the following list will combine pull downs of the same
+  ##bait but will still keep multiplicity of prey..i.e. if the
+  ##prey was found in 2 pulldowns, it will be recored twice 
+  xmlL <- split(unlist(xmlList), fac)
+  return(xmlL)
 }
