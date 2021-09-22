5abf139e474266ae167f780ea9a41f75bfad8872
LiNk-NY <marcel.ramosperez@roswellpark.org>
2021-09-22 17:59:02 +0000

    bug fix: condition length > 1 error

--- R/intAct2Sys.R
+++ R/intAct2Sys.R
@@ -1,25 +1,14 @@
-intAct2Sys <- function(prot2Sys, bpSysL){
+intAct2Sys <- function(prot2Sys, bpSysL) {
     options(error=recover)
-    if(!is.null(names(bpSysL[[prot2Sys]])) && !is.na(names(bpSysL[[prot2Sys]]))){
-
-        mapping <- intAct2Sys(names(bpSysL[[prot2Sys]])[1], bpSysL[[prot2Sys]])
-   }
-
-    else{
-
-        if(!is.null(bpSysL[[prot2Sys]]) && bpSysL[[prot2Sys]] != "ND"){
-
-            return(bpSysL[[prot2Sys]][1])
-
+    ind <- bpSysL[[prot2Sys]]
+    bpnames <- names(ind)
+    if (!all(is.null(bpnames)) && !all(is.na(bpnames))) {
+        intAct2Sys(names(bpSysL[[prot2Sys]])[1], bpSysL[[prot2Sys]])
+    } else {
+        if (!is.null(ind) && all(ind != "ND")) {
+            ind[1]
+        } else {
+            prot2Sys
         }
-
-        else{
-            return(prot2Sys)
-        }
-          
-        
     }
-
-    mapping
-    
 }
